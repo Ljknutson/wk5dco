a4bf8fcee9eed8597884ba64da3dc369a669aed3
Lisa Knutson <lisa.knutson@wmich.edu>
2022-03-29 19:10:37 -0400

    First commit

---
+++ web/sites/default/settings.lando.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$settings['hash_salt'] = 'wk5dco';
+
+$settings['config_sync_directory'] = '../config/sync';
+
+
+$databases['default']['default'] = array(
+
+  'database' => 'drupal9',
+
+  'username' => 'drupal9',
+
+  'password' => 'drupal9',
+
+  'host' => 'database',
+  'port' => 3306,
+
+  'driver' => 'mysql',
+
+);
